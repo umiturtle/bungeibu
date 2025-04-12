7c63c13783299e17575fb89c26405b884a029ffb
onigiri150291 <onigiri150291@gmail.com>
2025-04-12 12:41:43 +0000

    拡張

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'site';
+  constructor(){
+
+  }
+
 }
